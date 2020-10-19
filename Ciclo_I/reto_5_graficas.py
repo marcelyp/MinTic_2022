6f514138e094294224d120dea1c9a14528873f2f
AndresMpa <andresprieto2703@gmail.com>
2020-10-19 00:25:21 -0500

    Inicio el reto_5_graficas.py

---
+++ Ciclo_I/reto_5_graficas.py
@@ -0,0 +1,31 @@
+"""
+Descripción y  requerimiento:
+
+Como consultor técnico de la WHO (World Health Organization) se le ha solicitado que organice
+la información necesaria para realizaruna gráfica qué: por continente, muestre la evolución del
+promedio de la razón entre el número total de casos de COVID-19 y el número total de camas de
+hospital disponibles a través del tiempo.
+
+Para probar esta tarea, usted cuenta con el archivo de datos owid-covid-data.csv. Escriba una
+función que reciba como parámetro una cadena con la ruta dónde se encuentra guardado el archivo,
+incluyendo la extensión, y lo lea desde esta misma. A partir de estos datos, construya un
+dataframe sobre el cual, al utilizar el método df.plot() se obtenga la
+gráfica esperada
+"""
+
+import numpy as np
+import pandas as pd
+
+
+def caso_who(ruta_archivo_csv: str) -> dict:
+    print(ruta_archivo_csv[-4:len(ruta_archivo_csv)])
+    if ruta_archivo_csv[-4:len(ruta_archivo_csv)] != ".csv":
+        return "Extensión inválida."
+    try:
+        data = pd.read_csv(ruta_archivo_csv)
+        print(data.index("continent"))
+        return data.to_dict()
+    except:
+        return "Error al leer el archivode datos."
+
+caso_who("owid-covid-data.csv")
