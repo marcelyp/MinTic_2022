9cdcf1e46e028c0af128d267e069309a1d989f10
AndresMpa <andresprieto2703@gmail.com>
2020-10-22 23:43:30 -0500

    reto_5_graficas.py completo

--- Ciclo_I/reto_5_graficas.py
+++ Ciclo_I/reto_5_graficas.py
@@ -11,26 +11,53 @@ función que reciba como parámetro una cadena con la ruta dónde se encuentra g
 incluyendo la extensión, y lo lea desde esta misma. A partir de estos datos, construya un
 dataframe sobre el cual, al utilizar el método df.plot() se obtenga la
 gráfica esperada
+
+Valide que la extensión del nombre del archivo sea de tipo csv(coma separated values). En caso
+contrario, retornela siguiente cadena: “Extensión  inválida.”. Utilice un  bloque try except para
+leer el  archivo.  En  caso de  error, retornela siguiente cadena: “Error al leer el archivo de
+datos.”
+
+Tenga en cuenta las siguientes consideraciones:
+
+    • La fecha debe ser el índice principal del dataframe (No es una columna) y debe transformarse
+    aun objeto tipo fechade pandas
+    • El archivo de datos originalen sus registros NO cuenta con la información correspondiente al
+    total de númerode casos ni de camas de hospital disponibles ni la razón entre estos. Usted debe
+    realizar el cálculo correspondiente en función de las demás columnas que sí se encuentran en el
+    archivo original.
+    • Recuerde que su función no debe imprimir la gráfica sino retornar un diccionario.
+
+
 """
 
 import pandas as pd
+
+"""
+data[total_cases_per_million] * 10**3 / data[hospital_beds_per_thousand] * 10**6
+casos totales = (total_cases_per_million * population ) / 1000000
+total camas = ( hospital_beds_per_thousand * population) /1000
+razon = casostotales / total camas
+"""
 
 
 def caso_who(ruta_archivo_csv: str) -> dict:
     if ruta_archivo_csv[-4:len(ruta_archivo_csv)] != ".csv":
         return "Extensión inválida."
     try:
-        data = pd.read_csv(ruta_archivo_csv,
-                           na_values=["?"],
-                           index_col="date",
-                           columns=['first_name', 'last_name', 'age', 'amount_1', 'amount_2'])
-        data = pd.DataFrame(data)
-        data.index.name = "date"
-        data.columns.name = "continent"
-        print(data)
-        return data.to_dict()
+        covid_data = pd.read_csv(ruta_archivo_csv, na_values=["?"])
+        covid_data.dropna(subset=["continent"], inplace=True)
+
+        covid_data["total_cases"] = (covid_data["total_cases_per_million"] * covid_data["population"]) / 1_000_000
+        covid_data["beds"] = (covid_data["hospital_beds_per_thousand"] * covid_data["population"]) / 1_000
+        available = pd.Series(covid_data["total_cases"] / covid_data["beds"])
+
+        df_sum = pd.DataFrame({"date": pd.to_datetime(covid_data["date"]), "continent": covid_data["continent"],
+                               "available": available})
+        df_sum = df_sum.groupby(by=["date", "continent"])["available"].mean().reset_index()
+        df_sum = df_sum.pivot(index="date", columns="continent", values="available")
+        return df_sum.to_dict()
     except:
         return "Error al leer el archivode datos."
 
 
-caso_who("../Constantes/owid-covid-data.csv")
+print(caso_who("owid-covid-data.csv"))
